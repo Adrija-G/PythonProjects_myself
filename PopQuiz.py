d6b7e6c1ee71faad1a5528052be8711b0815af78
Adrija <108509197+Adrija-G@users.noreply.github.com>
2023-01-29 21:30:35 +0530

    Update PopQuiz.py

--- PopQuiz.py
+++ PopQuiz.py
@@ -1,72 +1,72 @@
-print("Wellcome to quiz game !!")
-print('NOTE: if your spelling is incorrect then it is considered as wrong answer')
+print("Welcome to the pop quiz!!")
+print('NOTE: If your spelling has to be correct or else, it will be considered as a wrong answer!')
 score = 0
 question_no = 0
 playing = input('Do you want to play ? ').lower()
 if playing == 'yes':
     question_no += 1
-    ques = input(f'\n{question_no}. what does API stand for? ').lower()
+    ques = input(f'\n{question_no}. What does API stand for? ').lower()
     if ques == 'Application Programming Interface':
         score +=1
-        print('correct! you got 1 point')
+        print('Correct! You got 1 point!')
         
     else:
         print('Incorrect!')
-        print(f'current answer is --> Application Programming Interface')
+        print(f'Correct answer is --> Application Programming Interface')
 
 # ------1
     question_no += 1
-    ques = input(f'\n{question_no}. what does GPU stand for? ').lower()
+    ques = input(f'\n{question_no}. What does GPU stand for? ').lower()
     
     if ques == 'graphics processing unit':
         score +=1
-        print('correct! you got 1 point')
+        print('Correct! You got 1 point!')
         
     else:
         print('Incorrect!')
-        print(f'current answer is --> Graphics Processing Unit')
+        print(f'Correct answer is --> Graphics Processing Unit')
 
 # -----2
     question_no += 1
-    ques = input(f'\n{question_no}. what does SEO stand for? ').lower()
+    ques = input(f'\n{question_no}. What does SEO stand for? ').lower()
     
     if ques == 'Search Engine Optimization':
         score +=1
-        print('correct! you got 1 point')
+        print('Correct! You got 1 point!')
         
     else:
         print('Incorrect!')
-        print(f'current answer is --> Search Engine Optimization')
+        print(f'Correct answer is --> Search Engine Optimization')
 
 # -----3
     question_no += 1
-    ques = input(f'\n{question_no}. what does HTML stand for? ').lower()
+    ques = input(f'\n{question_no}. What does HTML stand for? ').lower()
     
     if ques == 'Hyper Text Markup Language':
         score +=1
-        print('correct! you got 1 point')
+        print('Correct! You got 1 point')
         
     else:
         print('Incorrect!')
-        print(f'current answer is --> Hyper Text Markup Language')
+        print(f'Correct answer is --> Hyper Text Markup Language')
 
 
     question_no += 1
-    ques = input(f'\n{question_no}. what does ROM stand for? ').lower()
+    ques = input(f'\n{question_no}. What does ROM stand for? ').lower()
     
     if ques == 'Read Only memory':
         score +=1
-        print('correct! you got 1 point')
+        print('Correct! you got 1 point')
         
     else:
         print('Incorrect!')
-        print(f'current answer is --> Read Only memory')
+        print(f'Correct answer is --> Read Only memory')
 
 else:
     print('Thank you. You are out of the game.')
     quit()
 
-print(f'\nTotal Number of question is {question_no}')
+print(f'\nTotal Number of questions is {question_no}')
 print(f'Your score is {score}')
 try:
     percentage = (score *100)/question_no
